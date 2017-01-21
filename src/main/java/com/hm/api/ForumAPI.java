3f2ee4af0100cc6ab585edd16f23696795298dd8
Ninvnt <chipka222@gmail.com>
2017-01-21 14:36:30 +0200

    req

--- src/main/java/com/hm/api/ForumAPI.java
+++ src/main/java/com/hm/api/ForumAPI.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/forum")
 public class ForumAPI {
+    @RequestMapping("/login")
     public ResponseEntity login (@RequestParam("login") String login, @RequestParam("pass") String pass) {
         return ResponseEntity.ok(UserHolder.search(login, pass));
     }
